d28d2a611dbe5805e1fdee49c57dbb336d66ee4c
thamesssa <thamesssa@outlook.com>
2023-04-21 19:17:05 +0200

    initial commit

---
+++ instant-chat/src/App.jsx
@@ -0,0 +1,15 @@
+import Login from "./pages/Login"
+import Navbar from "./components/Navbar"
+
+function App() {
+
+
+  return (
+    <div>
+     <Navbar/>
+     <Login />
+    </div>
+  )
+}
+
+export default App
---
+++ instant-chat/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div><div className="navbar bg-primary text-primary-content">
+    <a className="btn btn-ghost normal-case text-xl">InstantApp</a>
+  </div></div>
+  )
+}
+
+export default Navbar 
